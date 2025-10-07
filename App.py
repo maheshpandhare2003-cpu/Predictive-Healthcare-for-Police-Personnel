166102bd06bfc11958bb169bf693cf7f2ac9a73f
maheshpandhare2003-cpu <maheshpandhare2003@gmail.com>
2025-10-07 12:52:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -9,9 +9,10 @@ st.markdown(
     <div style='text-align: center; padding: 10px; background-color: #1E3A8A; color: white; border-radius: 10px;'>
         <h1>Predictive Healthcare for Police Personnel</h1>
         <p>Get your personalized risk assessment and preventive suggestions</p>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Police_Icon.svg/1024px-Police_Icon.svg.png" 
+        <img src="—Pngtree—gold police officer badge_7258551.png" 
              width="80" alt="Police Logo">
     </div>
     """,
     unsafe_allow_html=True
 )
+
